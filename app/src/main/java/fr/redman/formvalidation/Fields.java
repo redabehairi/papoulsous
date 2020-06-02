c4160f115437759c39d38792389a5a7ff2b5902a
Réda Behairi <behairi.reda@gmail.com>
2020-06-02 23:16:05 +0200

    Formulaire résultat + styles.xml

--- app/src/main/java/fr/redman/formvalidation/Fields.java
+++ app/src/main/java/fr/redman/formvalidation/Fields.java
@@ -1,5 +1,5 @@
 package fr.redman.formvalidation;
 
 public enum Fields {
-    AMOUNT, RATE, LOAN_LENGTH, LOAN_PRICE, MONTHLY_PAYMENT;
+    AMOUNT, RATE, LOAN_LENGTH, LOAN_PRICE, MONTHLY_PAYMENT, INSURANCE_MONTHLY
 }
