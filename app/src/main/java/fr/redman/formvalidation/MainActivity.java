3d1868853e86637713bd6b865f37ea06a646e5ac
Réda Behairi <behairi.reda@gmail.com>
2020-06-13 00:51:33 +0200

    Ajout PTZ et ResultActivity

--- app/src/main/java/fr/redman/formvalidation/MainActivity.java
+++ app/src/main/java/fr/redman/formvalidation/MainActivity.java
@@ -5,26 +5,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
-import java.math.RoundingMode;
 
-import static java.math.BigDecimal.ROUND_HALF_UP;
 import static java.math.RoundingMode.HALF_UP;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Init
     EditText etAmount;
     EditText etRate;
     EditText etLoanLength;
-    EditText etLoanPrice;
-    EditText etMonthlyPayment;
+    EditText etPtz;
+    String loanPrice;
+    String monthlyPaymentNoInsurance;
+    String totalCost;
+    String monthlyTotal;
     Button btSubmit;
+    Button btClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,29 +36,28 @@ public class MainActivity extends AppCompatActivity {
         etAmount =findViewById(R.id.et_amount);
         etRate=findViewById(R.id.et_rate);
         etLoanLength=findViewById(R.id.et_loan_length);
-        etLoanPrice=findViewById(R.id.et_loan_price);
-        etMonthlyPayment=findViewById(R.id.et_monthly_payment);
+        etPtz =findViewById(R.id.et_ptz);
         btSubmit=findViewById(R.id.bt_submit);
-
-        btSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                computeLoanData();
-//                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
-//                intent.putExtra(Fields.AMOUNT.name(),Double.valueOf(etAmount.getText().toString()));
-//                intent.putExtra(Fields.RATE.name(),Double.valueOf(etRate.getText().toString()));
-//                intent.putExtra(Fields.LOAN_LENGTH.name(),Double.valueOf(etLoanLength.getText().toString()));
-//                intent.putExtra(Fields.LOAN_PRICE.name(),Double.valueOf(etLoanPrice.getText().toString()));
-//                intent.putExtra(Fields.MONTHLY_PAYMENT.name(),Double.valueOf(etMonthlyPayment.getText().toString()));
-//                startActivity(intent);
-            }
-        });
-
+        btClear=findViewById(R.id.bt_clear);
+        btClear.setOnClickListener(this);
+        btSubmit.setOnClickListener(this);
     }
 
     private void computeLoanData(){
         MathContext mc = new MathContext(10, HALF_UP);
-        BigDecimal amount = new BigDecimal(Double.valueOf(this.etAmount.getText().toString()),mc);
+        BigDecimal lengthBd = BigDecimal.valueOf(Integer.valueOf(this.etLoanLength.getText().toString())).multiply(BigDecimal.valueOf(12));
+        BigDecimal initialAmount = new BigDecimal(Double.valueOf(this.etAmount.getText().toString()),mc);
+        BigDecimal amount;
+        BigDecimal ptzAmount;
+        BigDecimal monthlyPtz=null;
+        if(this.etPtz.getText().length()>1){
+            ptzAmount = new BigDecimal(Double.valueOf(this.etPtz.getText().toString()),mc);
+            amount = initialAmount.subtract(ptzAmount);
+            monthlyPtz = ptzAmount.divide(lengthBd,mc);
+        }else{
+            amount = initialAmount;
+        }
+
         BigDecimal oneBd = new BigDecimal(1d,mc);
         BigDecimal annualRate = new BigDecimal(Double.valueOf(this.etRate.getText().toString()),mc).divide(new BigDecimal(100), mc);
 
@@ -68,19 +68,50 @@ public class MainActivity extends AppCompatActivity {
         BigDecimal periodicRate = new BigDecimal(periodicRateDouble,mc);
         periodicRate.setScale(10, HALF_UP);
 
-        BigDecimal lengthBd = BigDecimal.valueOf(Integer.valueOf(this.etLoanLength.getText().toString())).multiply(BigDecimal.valueOf(12));
 
         BigDecimal oneAndPeriodic = oneBd.add(periodicRate, mc);
         BigDecimal numerator = amount.multiply(periodicRate,mc);
         BigDecimal intermediateDenom = oneAndPeriodic.pow(-lengthBd.intValue(), mc);
         BigDecimal denominator = oneBd.subtract(intermediateDenom,mc);
 
-        BigDecimal monthlyPayment = numerator.divide(denominator, 10, HALF_UP);
+        BigDecimal monthlyPayment;
+        BigDecimal monthlyPaymentNoPTZ = numerator.divide(denominator, 10, HALF_UP);
+        if(monthlyPtz!=null){
+            monthlyPayment = monthlyPaymentNoPTZ.add(monthlyPtz,mc);
+        }else{
+            monthlyPayment = monthlyPaymentNoPTZ;
+        }
 
-        BigDecimal loanPrice = monthlyPayment.multiply(lengthBd,mc).subtract(amount, mc);
+        BigDecimal loanPrice = monthlyPaymentNoPTZ.multiply(lengthBd,mc).subtract(amount, mc);
 
-        etMonthlyPayment.setText(monthlyPayment.toString(), TextView.BufferType.EDITABLE);
-        etLoanPrice.setText(loanPrice.toString(), TextView.BufferType.EDITABLE);
+        this.monthlyPaymentNoInsurance = monthlyPayment.setScale(0,HALF_UP).toString().concat(" €");
+        this.totalCost = initialAmount.add(loanPrice,mc).setScale(0,HALF_UP).toString().concat(" €");
+        this.loanPrice = loanPrice.setScale(0,HALF_UP).toString().concat(" €");
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bt_clear:
+                this.etAmount.getText().clear();
+                this.etRate.getText().clear();
+                this.etLoanLength.getText().clear();
+                this.etPtz.getText().clear();
+                break;
+            case R.id.bt_submit:
+                executeComputingAndDisplayResult();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void executeComputingAndDisplayResult(){
+        computeLoanData();
+        Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+        intent.putExtra(Fields.MONTHLY_NO_INSURANCE.name(), monthlyPaymentNoInsurance);
+        intent.putExtra(Fields.LOAN_PRICE.name(),loanPrice);
+        intent.putExtra(Fields.TOTAL_COST.name(),totalCost);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/fr/redman/formvalidation/ResultActivity.java
+++ app/src/main/java/fr/redman/formvalidation/ResultActivity.java
@@ -1,17 +1,39 @@
 package fr.redman.formvalidation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultActivity extends AppCompatActivity {
 
+    TextView tvLoanPrice;
+    TextView tvMonthlyPayment;
+    TextView tvLoanInsurance;
+    TextView tvMonthlyInsurance;
+    TextView tvNotaryFees;
+    TextView tvTotal;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        Intent intent = getIntent();
+        //Assign varialbes
+        tvLoanPrice=findViewById(R.id.tv_loan_price);
+        tvMonthlyPayment=findViewById(R.id.tv_monthly_payment_no_insurance);
+        tvLoanInsurance =findViewById(R.id.tv_loan_insurance);
+        tvMonthlyInsurance=findViewById(R.id.tv_monthly_insurance);
+        tvNotaryFees=findViewById(R.id.tv_notary_fees);
+        tvTotal=findViewById(R.id.tv_total);
+
+        tvLoanPrice.setText(intent.getStringExtra(Fields.LOAN_PRICE.name()));
+        tvMonthlyPayment.setText(intent.getStringExtra(Fields.MONTHLY_NO_INSURANCE.name()));
+        tvTotal.setText(intent.getStringExtra(Fields.TOTAL_COST.name()));
+
 
     }
 }
